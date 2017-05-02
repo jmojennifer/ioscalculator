3014434861ddce401b7ab57d3466c0ef06af6f7b
Jennifer Owens <jmo.jennifer@gmail.com>
2017-05-02 07:53:43 -0700

    Finished lecture 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Xcode drops this into the method
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTyping = false
+    
     @IBAction func touchDigit(_ sender: UIButton) {
-        print("touchDigit was called")
+        let digit = sender.currentTitle!
+        
+        if userIsTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsTyping = true
+        }
     }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
+    
+    
     
 }
 
